1d09733633943e2ed790cd6caf5f5af21abedeca
sanghee-dev <leeesangheee@gmail.com>
2021-07-28 22:18:12 +0900

    Complete Login User Interface

--- Hello Chat/View/Authentication/LoginView.swift
+++ Hello Chat/View/Authentication/LoginView.swift
@@ -13,28 +13,68 @@ struct LoginView: View {
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                HStack { Spacer() }
-                
-                Text("Hello.")
-                    .font(.largeTitle)
-                    .bold()
-                
-                Text("Welcome Back")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(.blue)
+            VStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack { Spacer() }
+                    
+                    Text("Hello.")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    Text("Welcome Back")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.blue)
+                }
+                .padding(.leading)
                 
                 VStack(spacing: 20) {
                     TextField("Email", text: $email)
-                    
                     SecureField("Password", text: $password)
                 }
                 .padding([.top, .horizontal], 32)
                 
+                HStack {
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: Text("Reset password..."),
+                        label: {
+                            Text("Forgot Password?")
+                                .font(.system(size: 13, weight: .semibold))
+                                .foregroundColor(.blue)
+                                .padding(.top)
+                                .padding(.trailing, 32)
+                        })
+                }
+                
+                Button(action: {
+                    print("Handle sign up")
+                }, label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 330, height: 50)
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                .shadow(color: .gray, radius: 6, x: 0.0, y: 0.0)
+                
                 Spacer()
+                
+                NavigationLink(
+                    destination: RegistrationView(),
+                    label: {
+                        HStack {
+                            Text("Don't have an account?")
+                                .font(.system(size: 14))
+                            Text("Sign up")
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                    })
+                    .padding(.bottom, 32)
             }
-            .padding(.leading)
         }
         .padding(.top, -56)
     }
@@ -45,3 +85,4 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
+
