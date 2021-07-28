ee643b0fd0da785d377ff174f681674ff1cab870
sanghee-dev <leeesangheee@gmail.com>
2021-07-28 19:19:48 +0900

    Setup Project

---
+++ Hello Chat/View/Authentication/LoginView.swift
@@ -0,0 +1,38 @@
+//
+//  LoginView.swift
+//  Hello Chat
+//
+//  Created by leeesangheee on 2021/07/28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack { Spacer() }
+                
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.blue)
+                
+                Spacer()
+            }
+            .padding(.leading)
+        }
+        .padding(.top, -56)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
