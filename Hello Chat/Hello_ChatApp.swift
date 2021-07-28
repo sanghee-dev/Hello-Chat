fb0c189163642df0a45622096d0d322dd4709efa
sanghee-dev <leeesangheee@gmail.com>
2021-07-28 18:11:28 +0900

    Initial Commit

---
+++ Hello Chat/Hello_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_ChatApp.swift
+//  Hello Chat
+//
+//  Created by leeesangheee on 2021/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Hello_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
