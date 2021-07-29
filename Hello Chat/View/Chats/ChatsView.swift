1142202b20ead4364928221f6a343f2a778e6e08
sanghee-dev <leeesangheee@gmail.com>
2021-07-29 14:43:02 +0900

    Add Navigation Titles by using variable and onTapGesture func

--- Hello Chat/View/Chats/ChatsView.swift
+++ Hello Chat/View/Chats/ChatsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ChatsView: View {
     var body: some View {
         Text("Chats")
+            .navigationTitle("Chats")
     }
 }
 
--- Hello Chat/View/Main/MainTabView.swift
+++ Hello Chat/View/Main/MainTabView.swift
@@ -8,18 +8,42 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedIndex = 0
+    
+    var tabTitle: String {
+        switch selectedIndex {
+        case 0: return "Chats"
+        case 1: return "Channels"
+        case 2: return "Settings"
+        default: return ""
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            TabView {
+            TabView(selection: $selectedIndex) {
                 ChatsView()
+                    .onTapGesture {
+                        selectedIndex = 0
+                    }
                     .tabItem { Image(systemName: "bubble.left") }
+                    .tag(0)
                 
                 ChannelsView()
+                    .onTapGesture {
+                        selectedIndex = 1
+                    }
                     .tabItem { Image(systemName: "bubble.left.and.bubble.right") }
+                    .tag(1)
                 
                 SettingsView()
+                    .onTapGesture {
+                        selectedIndex = 2
+                    }
                     .tabItem { Image(systemName: "gear") }
+                    .tag(2)
             }
+            .navigationTitle(tabTitle)
         }
     }
 }
