c122793f7d92219daa73213a24d16e8623d69475
sanghee-dev <leeesangheee@gmail.com>
2021-08-02 21:25:21 +0900

    Setup Firebase

--- Hello Chat/Hello_ChatApp.swift
+++ Hello Chat/Hello_ChatApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Hello_ChatApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
