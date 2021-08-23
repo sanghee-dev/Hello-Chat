7baee35ee3d9598d51c7d86efefce9ca45eda271
sanghee-dev <leeesangheee@gmail.com>
2021-08-23 23:19:08 +0900

    Add condition to show MainTabView

--- Hello Chat/View/ContentView.swift
+++ Hello Chat/View/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         Group {
-            if viewModel.userSession != nil {
+            if (viewModel.currentUser != nil) {
                 MainTabView()
             } else {
                 LoginView()
